4c62cc83b3f409f79cf5b4b7f2115c64a4c6401e
KadoreK <haltaskadir@hotmail.com>
2023-03-08 00:08:46 +0300

    complex restriction create form created

--- Controllers/CommonController.cs
+++ Controllers/CommonController.cs
@@ -1,6 +1,7 @@
 ﻿using ExamSchedule.Models;
 using Microsoft.AspNetCore.Mvc;
 using Org.BouncyCastle.Asn1.X509.Qualified;
+using System.Linq;
 
 namespace ExamSchedule.Controllers
 {
@@ -13,38 +14,50 @@ namespace ExamSchedule.Controllers
         {
             _context = contex;
         }
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
 
         [HttpPost]
         public JsonResult GetEntityListForSchedule(long type, long scheduleId)
         {
+            JsonResult data = null;
             var entityType = (ComplexRestrictionTypeEnum)type;
-            var typeName = "";
-            //switch (entityType)
-            //{
-            //    case ComplexRestrictionTypeEnum.Teacher:
-            //        break;
-            //    case ComplexRestrictionTypeEnum.Course:
-            //        break;
-            //    case ComplexRestrictionTypeEnum.Student:
-            //        break;
-            //    case ComplexRestrictionTypeEnum.Programme:
-            //        break;
-            //    case ComplexRestrictionTypeEnum.Schedule:
-            //        break;
-            //    case ComplexRestrictionTypeEnum.Exam:
-            //        break;
-            //    default:
-            //        break;
-            //}
-            ///var list = _context.Set(entityType.ToString()).ToList();
+            dynamic list = null;
+            switch (entityType)
+            {
+                case ComplexRestrictionTypeEnum.Teacher:
 
+                    list = _context.Teachers.Select(x => new { val = x.Id, text = $"{x.Title} {x.Name} {x.LastName}" }).ToList();
+                    data = Json(list);
 
+                    break;
+                case ComplexRestrictionTypeEnum.Course:
 
-            return null;// Json(list);
+                    list = _context.Courses.Select(x => new { val = x.Id, text = $"{x.CourseShortFullName} {x.Name}" }).ToList();
+                    data = Json(list);
+                    break;
+                case ComplexRestrictionTypeEnum.Student:
+
+                    list = _context.Students.Select(x => new { val = x.Id, text = $"{x.Name} {x.LastName}" });
+                    data = Json(list);
+
+                    break;
+                case ComplexRestrictionTypeEnum.Programme:
+                    list = _context.Programmes.Select(x => new { val = x.Id, text = $"{x.Name}" });
+                    data = Json(list);
+                    break;
+                case ComplexRestrictionTypeEnum.Schedule:
+
+                    list = _context.Students.Select(x => new { val = x.Id, text = $"{x.Name} {x.LastName}" });
+                    data = Json(list);
+                    break;
+                case ComplexRestrictionTypeEnum.Exam:
+
+                    list = _context.Exams.Where(x=>x.ScheduleId==scheduleId).Select(x => new { val = x.Id, text = $"{x.Course.CourseShortFullName}" });
+                    data = Json(list);
+
+                    break;
+            }
+
+            return data;
         }
     }
 }
